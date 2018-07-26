0218760ae3ffce5b9446105ae406e15ce7f3f94d
jrmr <jeremierobles@gmx.fr>
2018-07-26 22:35:22 +0200

    new update
    add #s to display

---
+++ display.c
@@ -0,0 +1,127 @@
+#include "display.h"
+
+static void addBuffer(char input)
+{
+	*out++ = input;
+}
+
+extern void displayTest()
+{
+	putchar('\n');	
+	puts("JRMR DISPLAY");
+	putchar('\n');
+
+}
+
+extern void display(void* s, ...)
+{
+	char* string = s;
+	out = NULL;
+	va_list arg; // create you arg list
+	va_start( arg, s); // init to read past string (despues senor)
+	
+	int argNumber = get_sizeDisplay(string);
+	
+
+	for(int i=0; i < argNumber; i++)	//	checking all arg
+	{	
+		if( string[i] == '#')		//	check type
+		{
+			switch(string[i+1])	//	if it's true MAGICAL RUN
+			{
+				case 'c':		//	for char
+					i++;		//	move ++ in *s
+					putchar( (char) (va_arg(arg, int)) ); 	//putchar
+				
+		//			addBuffer( (char) (va_arg(arg, int)) );
+				break;
+				case 'd':			//	if it's int
+					i++;			//	move ++ in buffer (*s)
+					int temp = 0;
+					temp = va_arg(arg, void*);  // get int value
+					putInt(temp);		//	put int value
+					break;
+				case 's':
+					i++;
+					char* tempStr = NULL;
+					tempStr = va_arg(arg, void*);  // get int value
+					
+					int sizeStr = 0;
+					sizeStr = get_sizeDisplay( tempStr );
+					
+					for(int i=0; i<sizeStr; i++)
+					{
+						putchar( tempStr[i]);
+					}	
+
+					break;
+				default:
+					break;
+			}
+
+		}
+		else
+		{
+			putchar(string[i]);	//	just putchar for all buffer (exept #value)
+			//	addBuffer( string[i] );
+		}
+	}
+
+	//putchar('\n');		//	prout...
+	
+
+	//puts( out);
+	//putchar('\n');
+}
+
+
+static int get_sizeDisplay(char* string)	//	get size buffer
+{
+	int i;
+	for(i=0; *string; string++) i++;
+	return i;
+}
+
+void putInt(int string)		//	display PUTIIIN VIVA RUSSIA LOL (display int)
+{
+	if(string < 0)		//	reverse negative value to read more easy
+	{
+	      string *= -1;
+	      putchar('-');		//	but display negative value :)
+	      //addBuffer( '-' );
+	}
+
+	int i=1;			//	prepare count
+        int size = 0;		//	init size int (1, 10, 100 ect..)
+	
+	for(i=1; string-i > 10; i = i*10)	//	read size
+        {
+  	      size++;
+        }
+	
+	if(size > 2)	//	if it's a big size
+	{
+		
+		for(int b=0; b < (size); b++)
+		{
+			i /= 10;
+			putchar((int)(string/i)%10 + '0');
+			
+			//addBuffer( ((int)(string/i)%10 + '0') );	
+		}
+	}
+	else		//	if it's a little size
+	{
+		i *= 10; 		//	maths error to display an int with 1 or 2 size
+		for(int b=-1; b < (size); b++)
+		{
+  	                i /= 10;
+                	putchar((int)(string/i)%10 + '0');
+
+			//addBuffer( ((int)(string/i)%10 + '0') );
+		}
+	}
+
+}
+
+
---
+++ display.h
@@ -0,0 +1,42 @@
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+#include <stdio.h>
+#include <stdarg.h>	// multiple arg
+#include <string.h>
+
+#define symbole #
+
+/*
+ c=va_arg(ap,int); // get arg at position (i++)
+ va_list ap;	   // get all arg (it's a *) in ap
+ va_start(ap,a);   // init AT THE FIRST ARG !!!!!
+
+ thank's to: https://codes-sources.commentcamarche.net/faq/877-les-fonctions-a-nombre-variable-d-arguments
+
+
+Operation	Result	Note
+123 % 10	3	This gives the third/last digit
+123 / 10	12	The gives the remaining digits
+12 % 10	2	This gives the second digit
+12 / 10	1	This gives the remaining digits
+1 % 10	1	This gives the first digit
+1 / 10	0	This is when we stop
+
+ thank's to: https://wuhrr.wordpress.com/2007/11/09/how-to-print-a-long-integer-using-only-putchar/
+
+
+	 	just display int (#d), char(#c) and string(#s) 
+*/
+
+static char* out=NULL;		//	future surprise ^.^
+extern void displayTest();
+extern void display(void* string, ...);		// my little printf(work in progress..)
+
+
+static void putInt(int string);		//	putchar for int
+static int get_sizeDisplay(char* string);	//get size string
+static void addBuffer(char input);
+
+
+#endif
